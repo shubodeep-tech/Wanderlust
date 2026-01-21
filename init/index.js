ef2a7c4396f307b14ff57eaafda844ee18511b84
shubodeep-tech <shubodeep.k.das@gmail.com>
2026-01-21 09:36:11 +0530

    :Day 4 – Error Handling & Validation

--- init/index.js
+++ init/index.js
@@ -7,7 +7,6 @@ const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 async function main() {
   await mongoose.connect(MONGO_URL);
 }
-
 main()
   .then(() => {
     console.log("connected to db");
