c829e5e3ae99cd65fe5804010f5a0e32deacc5d1
shubodeep-tech <shubodeep.k.das@gmail.com>
2026-01-26 21:26:37 +0530

    Sessions, Cookies & Flash Messages

--- app.js
+++ app.js
@@ -5,11 +5,14 @@ const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const ExpressError = require("./utils/expressError.js");
+const session = require("express-session");
+const flash = require("connect-flash");
 
 const listings = require("./routes/listing.js");
 const reviews = require("./routes/reviews.js");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+
 
 
 // -------------------- DATABASE CONNECTION --------------------
@@ -32,11 +35,32 @@ app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.use(express.static(path.join(__dirname, "public")));
 
+const sessionOptions ={
+  secret: "mysupersecretcode",
+  resave :false,
+  saveUninitialized : true,
+  cookie :{
+    expires :Date.now() + 7 * 24 * 60 * 60 * 1000,
+    maxAge : 7* 24 * 60 * 60 * 1000,
+    httpOnly : true,
+
+  },
+};
 
 // -------------------- ROUTES --------------------
 app.get("/", (req, res) => {
   res.send("Hi, I am root");
 });
+
+app.use(session(sessionOptions));
+app.use(flash());
+
+app.use((req, res, next) => {
+  res.locals.success = req.flash("success");
+  res.locals.error = req.flash("error");
+  next();
+});
+
 
 app.use("/listings", listings);
 app.use("/listings/:id/reviews", reviews);
