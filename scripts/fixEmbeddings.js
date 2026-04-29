36ab3ce28138f63eade0a2fea22c8100acb1c562
shubodeep-tech <shubodeep.k.das@gmail.com>
2026-04-29 16:51:28 +0530

    Debuug  dotenv from running in production

---
+++ scripts/fixEmbeddings.js
@@ -0,0 +1,45 @@
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
+const mongoose = require("mongoose");
+const Listing = require("../models/listing");
+const { getEmbedding } = require("../services/embedding.service");
+
+async function fixEmbeddings() {
+  try {
+    await mongoose.connect(process.env.ATLASDB_URL);
+    console.log("MongoDB Connected");
+
+    console.log("DB NAME:", mongoose.connection.name);
+
+    const listings = await Listing.find();
+
+    for (let l of listings) {
+      const text = `${l.title} ${l.description} ${l.location} ${l.category}`;
+
+      const embedding = await getEmbedding(text);
+
+      if (!embedding || embedding.length === 0) {
+        console.log(" Failed:", l._id);
+        continue;
+      }
+
+      await Listing.updateOne(
+        { _id: l._id },
+        { $set: { embedding: embedding } }
+      );
+
+      console.log(" Updated:", l._id);
+    }
+
+    console.log(" All embeddings updated");
+    process.exit(0);
+
+  } catch (err) {
+    console.error(" Error:", err);
+    process.exit(1);
+  }
+}
+
+fixEmbeddings();
