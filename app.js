902571b71dd791f3f2d03bdd7f4880761f4738e7
Shubodeep <shubodeep.k.das@gmail.com>
2026-05-02 14:51:36 +0530

    Add secure and sameSite options to cookies

--- app.js
+++ app.js
@@ -57,6 +57,8 @@ app.use(
     saveUninitialized: false,
     cookie: {
       httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "lax",
       maxAge: 1000 * 60 * 60 * 24 * 7,
     },
   })
