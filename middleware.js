761becdcf518ee0f07e0fc33230b8cf60d714d6f
Shubodeep <shubodeep.k.das@gmail.com>
2026-05-05 18:41:50 +0530

    saving in isLoggedIn middleware

--- middleware.js
+++ middleware.js
@@ -5,7 +5,7 @@ const { listingSchema, reviewSchema } = require("./schema.js");
 
 module.exports.isLoggedIn = (req, res, next) => {
   if (!req.isAuthenticated()) {
-    req.session.redirectUrl = req.originalUrl; // ✅ save where they were going
+    req.session.redirectUrl = req.originalUrl; 
     req.flash("error", "You must be logged in first!");
     return res.redirect("/login");
   }
